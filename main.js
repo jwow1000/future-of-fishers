7211084c5b822241a4b046de535dfc26b76e86e8
Jeremy Wiles-Young <jwilesyoung@gmail.com>
2025-03-09 19:40:12 -0400

    pretty good

--- main.js
+++ main.js
@@ -2,25 +2,41 @@ window.Webflow ||= [];
 window.Webflow.push(() => {
   let focusState = false;
   let currentFocus = null;
+  const blurLayer =  document.querySelector(".fof-blur-layer");
+  const mainWrapper = document.querySelector(".wrapper-fof");
+  
+  mainWrapper.addEventListener("click", (event) => {
+    // console.log("click event", event.target);
+    
+    const target = event.target;
+    const targetId = target.getAttribute("data-id");
 
-  // Add click events to gallery items
-  const images = document.querySelectorAll(".w-dyn-items [data-id]");
-  images.forEach((image) => {
-    image.addEventListener("click", (e) => {
-      console.log("tag", e.target);
-
-      // Get data-id instead of id
-      const targetId = e.target.getAttribute("data-id");
-      if (!focusState) {
-        const match = document.querySelector(`.fof-text-wrapper[data-id="${targetId}"]`);
-        currentFocus = match;
-        currentFocus.style.display = 'block';
-        focusState = true;
-      } else {
-        currentFocus.style.display = 'none';
+    // if focusState is true, any click closes the focus
+    if( focusState ) {
+      // close the focus state
+      if( currentFocus ) {
+        blurLayer.style.opacity = '0';
+        blurLayer.style.pointerEvents = 'none';
+        currentFocus.style.display = 'none'; // Hide the previously focused element
+        currentFocus.offsetHeight; // Force reflow
         focusState = false;
       }
+    } else {
+     if( targetId ) {
+        // if no focus and target Id is true set new current focus and transition
+        event.stopPropagation();
+        const match = document.querySelector(`.fof-text-wrapper[data-id="${targetId}"]`);
+        currentFocus = match;
+        blurLayer.style.opacity = '1';
+        blurLayer.style.pointerEvents = 'auto';
+        // currentFocus.style.display = 'block'; // Show the new focused element
+        requestAnimationFrame(() => {
+          currentFocus.style.display = "block";
+          currentFocus.offsetHeight; // Force reflow
+        });
+        focusState = true; 
+      }   
+    }
 
-    });
   });
 });
