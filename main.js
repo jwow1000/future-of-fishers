52c770606fe89e499668b6748f1fb26aba373b6c
Jeremy Wiles-Young <jwilesyoung@gmail.com>
2025-03-08 22:31:27 -0500

    better id matching

--- main.js
+++ main.js
@@ -1,15 +1,26 @@
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  let focusState = false
+  let focusState = false;
+  let currentFocus = null;
 
-  // add click events to gallery items
-  const images = document.querySelectorAll(".w-dyn-items");
+  // Add click events to gallery items
+  const images = document.querySelectorAll(".w-dyn-items [data-id]");
   images.forEach((image) => {
     image.addEventListener("click", (e) => {
       console.log("tag", e.target);
-      image.classList.add('fof-focus');
-      
+
+      // Get data-id instead of id
+      const targetId = e.target.getAttribute("data-id");
+      if (!focusState) {
+        const match = document.querySelector(`.fof-text-wrapper[data-id="${targetId}"]`);
+        currentFocus = match;
+        currentFocus.style.display = 'block';
+        focusState = true;
+      } else {
+        currentFocus.style.display = 'none';
+        focusState = false;
+      }
+
     });
   });
-
 });
